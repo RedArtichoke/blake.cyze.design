5cff6150619748a7d03ddff28cadcad0786df779
Red Artichoke <bcyze@icloud.com>
2021-01-26 17:19:10 -0500

    Init

---
+++ ui/src/App.svelte
@@ -0,0 +1,103 @@
+<script>
+  let width;
+</script>
+
+<style></style>
+
+<svelte:window bind:innerWidth={width} />
+
+<header>
+  <div class="cover bg-left bg-center-l" style="background-image: url(IMG_3353.jpeg)">
+    <div class="bg-black-60 pb5 pb6-m pb7-l">
+      <nav class="dt w-100 mw8 center">
+        <div class="dtc w2 v-mid pa3"></div>
+      </nav>
+      <div class="tc-l mt4 mt5-m mt7-l ph3">
+        <h1 class="f2 f1-l white-90 mb0 lh-title">Hi!  I'm Blake.</h1>
+      </div>
+    </div>
+  </div>
+</header>
+<main class="App mw9 mw8-ns center pa3 ph5-ns lh-copy">
+  <div class="flex items-center">
+    <div class="flex-grow-1">
+      <h2>A bit about me...</h2>
+      <p>
+        I live in
+        beautiful <a href="https://www.google.com/maps/place/Kincardine,+ON/">Kincardine, Ontario</a>.
+        I love visual design and programming and learning new things :)
+      </p>
+      <p>
+        I am in my second term of
+        my first year at <a href="https://www.csd.uwo.ca/">Western University in Computer Science with a minor in design</a>.  It has been a
+        challenging school year so far with Covid-19 impacting how universities deliver courses.  So far so good,
+        I have proven that I am capable of working with minimal supervision remotely - both for school and work
+        (my last summer job at Dairy Queen was partly remote).
+      </p>
+      <h2>Applications I use for design...</h2>
+      <p>
+        I have had an Adobe Creative Cloud subscription for over five years, and, in that time,
+        I have been a regular user of Illustrator, Photoshop
+        and Premiere for various school & personal projects.  Lately I have been learning <a href="https://www.figma.com/">Figma</a> - it looks great
+        for user interface design and collaboration.  My design class at university uses Affinity Designer and Photo.  It's
+        a great low budget option, but I feel like I have been spoiled from using Photoshop and Illustrator for so long!
+      </p>
+      <h2>For presentations & prototyping...</h2>
+      <p>
+        I am at home using both PowerPoint and Keynote.  And I am completely
+        comfortable with MS Office and Pages.  Or drawing on a napkin.  Whatever it takes.
+      </p>
+      <h2>Programming...</h2>
+      <p>
+        I spent last summer designing a complicated Excel spreadsheet to manage inventory at my local Dairy Queen.
+        This school year I have been writing school assignments in Python and Java.  I enjoy programming
+        as much as I enjoy visual design.
+      </p>
+      <h2>For Fun...</h2>
+      <p>
+        I am a big gaming nerd (at one point I was a world ranked player of
+        <a href="https://www.teamfortress.com/">Team Fortress 2</a>), and it was this
+        passion for gaming that led me to pursue an education and career in design and computers.
+      </p>
+      <h2>Here are some examples of my art & design...</h2>
+      <h3>Manga</h3>
+      <p>
+        I own an iPad Pro + Pencil, and I use
+        <a href="https://procreate.art/ipad">ProCreate</a> for most of my drawings.
+      </p>
+
+      <img src="967699855.jpeg" class="mw6"/>
+      <img src="3682124570.jpeg" class="mw6"/>
+      <img src="1752384821.jpeg" class="mw6"/>
+
+      <h3>Photography</h3>
+      <img src="IMG_3353.jpeg" class="mw6"/>
+      <img src="75523252_496569940974383_109227630121356022_n.jpg" class="mw6"/>
+      <img src="83199138_1771318866338617_1986542429855547648_n.jpg" class="mw6"/>
+
+      <h3>Logos & User Interface Design</h3>
+      <p>
+        This is a logo I designed for a hackathon app I built with some friends.  The
+        application is for Alzheimer's patients who are suffering from memory loss.
+        Patients can open this app and get reminders of important people in their lives,
+        and other details they might forget.
+      </p>
+      <img src="unknown-1.jpeg" class="mw6"/>
+      <img src="unknown.jpeg" class="mw6"/>
+
+      <h3>Charcoal Drawings</h3>
+      <p>
+        These drawings are getting a bit old now (I made them when I was 15), but
+        hopefully they convey my interest in art from a young age.
+      </p>
+      <img src="IMG_2884.jpeg" class="mw6"/>
+      <img src="IMG_2881.jpeg" class="mw6"/>
+      <p>
+        Ok, not charcoal... and this is REALLY early mixed-media work.  Proof I have
+        been an aspiring artist since I was *very* young!
+      </p>
+      <img src="IMG_2882.jpeg" class="mw6"/>
+
+    </div>
+  </div>
+</main>
